1b018505d5fc559ecfcae7d29e8134e2d30c57ef
nnoldman <1527053436@qq.com>
2017-07-18 15:19:32 +0800

    整理，删除sceneapp

--- Core/NetWork.h
+++ Core/NetWork.h
@@ -10,53 +10,49 @@
 #include "ConfigInfo.h"
 #include "Poco/Mutex.h"
 
-class CX_LIB NetWork:uCallBack
-{
-public:
-	uDelegate onMessage;
-	uDelegate onDisconnect;
-	uDelegate onConnect;
-public:
-	struct MsgArgs :public uEventArgs
-	{
-		PKG* pkg;
-		Connection* connect;
-		MsgArgs() :pkg(nullptr), connect(nullptr){
-		}
-	};
-	struct ConnectArg:uEventArgs 
-	{
-		Connection* connect;
-		ConnectArg(){
-		}
-	};
-	struct Packet 
-	{
-		u32 len;
-		char* data;
-		Connection* con;
-		Packet() :data(nullptr), con(nullptr), len(0) {
-		};
-		~Packet(){
-			delete[] data;
-		}
-	};
-public:
-	NetWork();
-	~NetWork();
+class CX_LIB NetWork : uCallBack {
+  public:
+    uDelegate onMessage;
+    uDelegate onDisconnect;
+    uDelegate onConnect;
+  public:
+    struct MsgArgs : public uEventArgs {
+        PKG* pkg;
+        Connection* connect;
+        MsgArgs() : pkg(nullptr), connect(nullptr) {
+        }
+    };
+    struct ConnectArg : uEventArgs {
+        Connection* connect;
+        ConnectArg() {
+        }
+    };
+    struct Packet {
+        u32 len;
+        char* data;
+        Connection* con;
+        Packet() : data(nullptr), con(nullptr), len(0) {
+        };
+        ~Packet() {
+            delete[] data;
+        }
+    };
+  public:
+    NetWork();
+    ~NetWork();
 
-	bool initialize(const NetConfig& config);
+    bool initialize(const NetConfig& config);
 
-	void prosess();
+    void prosess();
 
-	void addMessage(void* data, u32 len, Connection* con);
+    void addMessage(void* data, u32 len, Connection* con);
 
-private:
+  private:
 
-	Poco::Mutex mLock;
+    Poco::Mutex mLock;
 
-	uArray<Packet*> mDataArray;
+    uArray<Packet*> mDataArray;
 
-	Poco::Net::TCPServer* mServer;
+    Poco::Net::TCPServer* mServer;
 };
 
